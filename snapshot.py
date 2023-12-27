273cb6416d67880a730d44a3bc999538ffe9e188
ganeshmerwade <125240167+ganeshmerwade@users.noreply.github.com>
2023-12-27 18:19:30 +0530

    Update snapshot.py

--- snapshot.py
+++ snapshot.py
@@ -4,48 +4,78 @@
 import boto3
 import traceback
 import time
+from datetime import datetime, timedelta
 def lambda_handler(event, context):
     ec2 = boto3.client('ec2')
+    ec2resource = boto3.resource('ec2')
     
     # Get all in-use volumes in all regions
-    result = ec2.describe_volumes( Filters=[{'Name': 'status', 'Values': ['in-use']}, {'Name': 'tag:Backup', 'Values': ['15-days']}])
+    result = ec2.describe_volumes( Filters=[{'Name': 'status', 'Values': ['in-use']}, {'Name': 'tag-key', 'Values': ['Backup']}])
     report = {}
     for volume in result['Volumes']:
         try:
-            print(f"Backing up {volume['VolumeId']} in {volume['AvailabilityZone']}")
-            
-            # Create snapshot
-            result = ec2.create_snapshot(VolumeId=volume['VolumeId'],Description='Created by Lambda backup function ebs-snapshots')
-            
-            # Get snapshot resource
-            ec2resource = boto3.resource('ec2')
-            snapshot = ec2resource.Snapshot(result['SnapshotId'])
-                        
+            volume_id = volume['VolumeId']
             # Find name tag for volume
             if 'Tags' in volume:
                 for tags in volume['Tags']:
                     if tags["Key"] == 'Name':
-                        volumename = tags["Value"]
+                        volume_name = tags["Value"]
             else:
-                volumename = 'N/A'
+                volume_name = volume['VolumeId']
+                 
+            if 'Tags' in volume:
+                for tags in volume['Tags']:
+                    if tags["Key"] == 'Backup':
+                        frequency = tags["Value"]    
+            
+            # Find last snaphot for the volume        
+            response = ec2.describe_snapshots(Filters=[{'Name': 'volume-id', 'Values': [volume_id]}])
+            sorted_response = sorted(response['Snapshots'], key=lambda x: x['StartTime'], reverse=True)
+            
+            if sorted_response:
+                # latest_snapshot_id = sorted_response[0]['SnapshotId']
+                ec2resource = boto3.resource('ec2')
+                snapshot = ec2resource.Snapshot(sorted_response[0]['SnapshotId'])
+                time_difference = datetime.now() - snapshot.start_time.replace(tzinfo=None)
+                days_difference = time_difference.days
+                
+                if days_difference > int(frequency):
+                    result = ec2.create_snapshot(VolumeId=volume['VolumeId'],Description='Created by Lambda backup function ebs-snapshots')
+                    snap = ec2resource.Snapshot(result['SnapshotId'])
+                    
+                    # Add volume name to snapshot for easier identification
+                    snap.create_tags(Tags=[{'Key': 'Name','Value': volume_name}])
+                    
+                    #add retention period in tags
+                    retention_period = int(frequency) + 1
 
-            #wait for snapshot to complete
-            while snapshot.state == 'pending':
-                print(f"{volumename}: {snapshot.state}")
-                time.sleep(1)
-                snapshot.reload()
-
-            if snapshot.state == 'completed':
-
+                    snap.create_tags(Tags=[{'Key': 'RetentionTime','Value': str(retention_period)}])
+                    report[volume_name]='SUCCESS'
+                else:
+                    print(f"snapshot for {volume_name} is created {days_difference} days ago")    
+            else:         
+            
+                print(f"Backing up {volume['VolumeId']} in {volume['AvailabilityZone']} for first time")
+            
+                # Create snapshot
+                result = ec2.create_snapshot(VolumeId=volume['VolumeId'],Description='Created by Lambda backup function ebs-snapshots')
+            
+                # Get snapshot resource
+                snapshot = ec2resource.Snapshot(result['SnapshotId'])
+                            
                 # Add volume name to snapshot for easier identification
-                snapshot.create_tags(Tags=[{'Key': 'Name','Value': volumename}])
+                snapshot.create_tags(Tags=[{'Key': 'Name','Value': volume_name}])
 
-                report[volumename]='SUCCESS'
-            else:
-                report[volumename]='FAILED'
+                #add retention period in tags
+                retention_period = int(frequency) + 1
+
+                snapshot.create_tags(Tags=[{'Key': 'RetentionTime','Value': str(retention_period)}])
+
+                report[volume_name]='SUCCESS'
+
         except Exception as e: 
-            report[volumename]='FAILED'
-            print(f"{volumename}")
+            report[volume_name]='FAILED'
+            print(f"{volume_name}")
             print(e)
             traceback.print_exc()
 
