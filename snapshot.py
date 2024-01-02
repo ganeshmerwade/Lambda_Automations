f25a798275bc1a2c7cace2cbf4478cd9e384ebd5
ganeshmerwade <125240167+ganeshmerwade@users.noreply.github.com>
2024-01-02 18:34:28 +0530

    Update snapshot.py

--- snapshot.py
+++ snapshot.py
@@ -1,44 +1,47 @@
-# Backup all in-use volumes in all regions
-# Detailed instructions available on 
-#https://medium.com/@codebyamir/automate-ebs-snapshots-using-aws-lambda-cloudwatch-1e2acfb0a45a#:~:text=In%20the%20Lambda%20console%2C%20go%20to%20Functions%20%3E,all%20regions%20import%20boto3%20def%20lambda_handler%20%28event%2C%20context%29%3A
+# This script will create a snapshot of volumes that match a certain filter based on tags which match [Backup]
+# Creating the snapshot is determined based on a Backup tag (provided for volumes through terraform) and the date of the last snapshot in AWS.
+# This scipt requires python 3.x 
+# Additional information: https://medium.com/@codebyamir/automate-ebs-snapshots-using-aws-lambda-cloudwatch-1e2acfb0a45a#:~:text=In%20the%20Lambda%20console%2C%20go%20to%20Functions%20%3E,all%20regions%20import%20boto3%20def%20lambda_handler%20%28event%2C%20context%29%3A
+
 import boto3
 import traceback
 import time
 from datetime import datetime, timedelta
-def lambda_handler(event, context):
-    ec2 = boto3.client('ec2')
-    ec2resource = boto3.resource('ec2')
-    
-    # Get all in-use volumes in all regions
-    result = ec2.describe_volumes( Filters=[{'Name': 'status', 'Values': ['in-use']}, {'Name': 'tag-key', 'Values': ['Backup']}])
-    report = {}
-    for volume in result['Volumes']:
-        volume_id = volume['VolumeId']
-        volume_name = volume['VolumeId']
-        # Find name tag for volume
-        if 'Tags' in volume:
-            for tags in volume['Tags']:
+
+def get_volume_name(volume_obj):
+    vol_name = volume_obj['VolumeId']
+    if 'Tags' in volume_obj:
+            for tags in volume_obj['Tags']:
                 if tags["Key"] == 'Name':
-                    volume_name = tags["Value"]
-        try:                        
-            if 'Tags' in volume:
-                for tags in volume['Tags']:
-                    if tags["Key"] == 'Backup':
-                        frequency = tags["Value"]    
+                    vol_name = tags["Value"]
+
+    return vol_name                
+
+def get_backup_frequency_value(volume_obj):
+    if 'Tags' in volume:
+        for tags in volume['Tags']:
+            if tags["Key"] == 'Backup':
+                frequency = tags["Value"]
+    return frequency
+
+def find_latest_snapshot(ec2client_obj, volume_obj):
+    response = ec2client_obj.describe_snapshots(Filters=[{'Name': 'volume-id', 'Values': [volume_obj['VolumeId']]}])
+    sorted_response = sorted(response['Snapshots'], key=lambda x: x['StartTime'], reverse=True)
+
+    return sorted_response
+
+def create_snapshot(volume_obj, ec2client_obj, ec2resource_obj, vol_name,  report_dict):
+    try:                             
+            backup_frequency = get_backup_frequency_value(volume_obj)        
             
-            # Find last snaphot for the volume        
-            response = ec2.describe_snapshots(Filters=[{'Name': 'volume-id', 'Values': [volume_id]}])
-            sorted_response = sorted(response['Snapshots'], key=lambda x: x['StartTime'], reverse=True)
-            
-            if sorted_response:
-                # latest_snapshot_id = sorted_response[0]['SnapshotId']
-                ec2resource = boto3.resource('ec2')
-                snapshot = ec2resource.Snapshot(sorted_response[0]['SnapshotId'])
+            latest_snapshot = find_latest_snapshot(ec2client_obj, volume_obj)            
+            if latest_snapshot:
+                snapshot = ec2resource_obj.Snapshot(latest_snapshot[0]['SnapshotId'])
                 time_difference = datetime.now() - snapshot.start_time.replace(tzinfo=None)
                 days_difference = time_difference.days
                 
                 if days_difference > int(frequency):
-                    result = ec2.create_snapshot(VolumeId=volume['VolumeId'],Description='Created by Lambda backup function ebs-snapshots')
+                    result = ec2client_obj.create_snapshot(VolumeId=volume['VolumeId'],Description='Created by Lambda backup function ebs-snapshots')
                     snap = ec2resource.Snapshot(result['SnapshotId'])
                     
                     # Add volume name to snapshot for easier identification
@@ -76,6 +79,18 @@ def lambda_handler(event, context):
             print(f"{volume_name}")
             print(e)
             traceback.print_exc()
+
+def lambda_handler(event, context):
+    ec2_client = boto3.client('ec2')
+    ec2_resource = boto3.resource('ec2')
+    
+    # Get all in-use volumes in all regions
+    result = ec2_client.describe_volumes( Filters=[{'Name': 'status', 'Values': ['in-use']}, {'Name': 'tag-key', 'Values': ['Backup']}])
+    report = {}
+    for volume_data in result['Volumes']: 
+        get_volume_name =  get_volume_name(volume_data)
+
+        
 
     for key,value in report.items():
         print(f"{key}: {value}")
